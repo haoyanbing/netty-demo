4c6ac6a94db39a1b0c1063c62ea3d7be2032c82d
hao.yanbing <binary_hao@163.com>
2020-04-11 16:41:18 +0800

    init

---
+++ src/main/java/com/haoyanbing/netty/DiscardServer.java
@@ -0,0 +1,49 @@
+package com.haoyanbing.netty;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+/**
+ * @author haoyanbing
+ * @since 2020/4/11
+ */
+public class DiscardServer {
+    public void bind(int port) throws InterruptedException {
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            serverBootstrap.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .option(ChannelOption.SO_BACKLOG, 1024)
+                    .childHandler(new ChildChannelHandler());
+
+            ChannelFuture future = serverBootstrap.bind(port).sync();
+
+            future.channel().closeFuture().sync();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+
+
+    private class ChildChannelHandler extends ChannelInitializer<SocketChannel> {
+
+        @Override
+        protected void initChannel(SocketChannel socketChannel) throws Exception {
+            socketChannel.pipeline().addLast(new DiscardServerHandler());
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        new DiscardServer().bind(8080);
+    }
+
+}
---
+++ src/main/java/com/haoyanbing/netty/DiscardServerHandler.java
@@ -0,0 +1,47 @@
+package com.haoyanbing.netty;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+/**
+ * @author haoyanbing
+ * @since 2020/4/11
+ */
+public class DiscardServerHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+//        ByteBuf buf = (ByteBuf) msg;
+//        try {
+//            while (buf.isReadable()) {
+//                System.out.println((char) buf.readByte());
+//                System.out.flush();
+//            }
+//        } finally {
+//            ReferenceCountUtil.release(msg);
+//        }
+        ctx.write(msg);
+        ctx.flush();
+    }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        final ByteBuf time = ctx.alloc().buffer(4);
+        time.writeInt((int) (System.currentTimeMillis() / 1000L + 2208988800L));
+
+        final ChannelFuture f = ctx.writeAndFlush(time);
+        f.addListener((ChannelFutureListener) future -> {
+            assert f == future;
+            ctx.close();
+        });
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
